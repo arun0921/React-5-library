235a64fa4751c292ddee245d2314af9c4efac46d
arun0921 <arunsarun9442@gmail.com>
2023-07-18 21:09:14 +0530

    first commit

---
+++ src/navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+      <div className="navbar w-full bg-gray-400 p-3 flex justify-center items-center">
+        <div className="title text-black">
+          Library mangement system 
+        </div>
+      </div>
+    );
+  };
+  export default Navbar;
